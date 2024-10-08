d9ed93700374b3c59e06b5654e23828d150898e2
jacksonorris <jack.norri@gmail.com>
2024-10-07 20:32:43 -0600

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -2,7 +2,7 @@ import random
 arr = [random.randint(1,100) for x in range(random.randint(1,10))]
 
 def insertion_sort(arr):   
-   for x in range(len(arr)-1):      
+   for x in range(len(arr)):      
       small = x
       
       while small > 0 and arr[small-1] > arr[small]:
